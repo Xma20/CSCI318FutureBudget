997e6da354028167190221149054af449a735240
DOleaga <doleaga@nyit.edu>
2022-04-27 01:40:39 -0400

    Added to about us page.

---
+++ CSCI318FutureBudget/AboutUs.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="FutureBudget.css?v=<?php echo time(); ?> " >
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Future Budget: About Us</title>
+
+    <?php
+        include('header.php');
+    ?>
+</head>
+<body>
+    <h1> About Us </h1>
+
+    <h2>
+        The Future Budget Team was founded in 2022 as our team wanted to address budgeting issues within our company.
+        As discussions sparked, we had came to the realization that the company's spending habits are influenced greatly
+        by the individuals that make it up. To confront this issue, we decided to create a financial management tracker 
+        for our company as well as for everyday use.
+    </h2>
+
+    <h2>
+        Future Budget is dedicated to helping users save as much as possible for that rainy day.
+        As nice as it is to be subscribed to every streaming platform out there for exclusives, those bills can pile up.
+        Along with these are essential bills that eat away at your paycheck in no time.
+        In order to keep track of everything in one place, we provide an interactive environment for users
+        to modify their transactional views as they please.
+     </h2>
+</body>
+
+<?php
+        include('footer.php');
+?>
+
+
+</html>
